d4b86d30548e7252a21ea0d23751e3872561da02
Simo Kinnunen <simo@shoqolate.com>
2014-02-24 19:36:06 +0900

    Allow options to be passed to InputAgent.

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ set -xe
 pkg=jp.co.cyberagent.stf.input.agent
 cls=InputAgent
 apk=$(adb shell pm path $pkg | dos2unix | cut -c9-)
-adb shell "export CLASSPATH=$apk; exec app_process /system/bin $pkg.$cls"
+adb shell "export CLASSPATH=$apk; exec app_process /system/bin $pkg.$cls" $@
