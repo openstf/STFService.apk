63fc5c84ab048130c44efbe63e82373f2aa7e3eb
Simo Kinnunen <simo@shoqolate.com>
2014-02-27 20:08:30 +0900

    Allow InputService to keep a wake lock alive so that the display can be put offline when it's not being used.

--- lib/src/main/java/jp/co/cyberagent/stf/input/agent/InputService.java
+++ lib/src/main/java/jp/co/cyberagent/stf/input/agent/InputService.java
@@ -1,32 +1,78 @@
 package jp.co.cyberagent.stf.input.agent;
 
-import android.app.IntentService;
 import android.app.KeyguardManager;
+import android.app.Service;
 import android.content.Intent;
+import android.os.IBinder;
+import android.os.PowerManager;
 import android.util.Log;
 
-public class InputService extends IntentService {
+public class InputService extends Service {
     private static final String TAG = "InputService";
-    private static final String UNLOCK_ACTION = "jp.co.cyberagent.stf.input.agent.Unlock";
+    private static final String ACTION_UNLOCK = "jp.co.cyberagent.stf.input.agent.InputService.ACTION_UNLOCK";
+    private static final String ACTION_WAKE_LOCK_ACQUIRE = "jp.co.cyberagent.stf.input.agent.InputService.ACTION_WAKE_LOCK_ACQUIRE";
+    private static final String ACTION_WAKE_LOCK_RELEASE = "jp.co.cyberagent.stf.input.agent.InputService.ACTION_WAKE_LOCK_RELEASE";
+    private PowerManager powerManager;
+    private PowerManager.WakeLock wakeLock;
 
-    public InputService() {
-        super("InputService");
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 
     @Override
-    protected void onHandleIntent(Intent intent) {
-        if (intent.getAction().equals(UNLOCK_ACTION)) {
-            Log.i(TAG, "Unlocking device");
+    public void onCreate() {
+        super.onCreate();
+        powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        releaseWakeLock();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        String action = intent.getAction();
+        if (ACTION_UNLOCK.equals(action)) {
             unlock();
         }
-        else {
-            Log.e(TAG, "Unknown action " + intent.getAction());
+        else if (ACTION_WAKE_LOCK_ACQUIRE.equals(action)) {
+            acquireWakeLock();
         }
+        else if (ACTION_WAKE_LOCK_RELEASE.equals(action)) {
+            releaseWakeLock();
+        }
+        else {
+            Log.e(TAG, "Unknown action " + action);
+        }
+        return START_STICKY;
     }
 
     @SuppressWarnings("deprecation")
     private void unlock() {
+        Log.i(TAG, "Unlocking device");
         KeyguardManager keyguardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
-        keyguardManager.newKeyguardLock("InputService/Unlock").disableKeyguard();
+        keyguardManager.newKeyguardLock("InputService").disableKeyguard();
+    }
+
+    @SuppressWarnings("deprecation")
+    private void acquireWakeLock() {
+        releaseWakeLock();
+        Log.i(TAG, "Acquiring wake lock");
+        wakeLock = powerManager.newWakeLock(
+                PowerManager.SCREEN_DIM_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP,
+                "InputService"
+        );
+        wakeLock.acquire();
+    }
+
+    private void releaseWakeLock() {
+        if (wakeLock != null) {
+            Log.i(TAG, "Releasing wake lock");
+            wakeLock.release();
+            wakeLock = null;
+        }
     }
 }
