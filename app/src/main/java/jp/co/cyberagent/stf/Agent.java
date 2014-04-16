46e7e37da7d613c8f26e47e823a137aec3cf173a
Simo Kinnunen <simo@shoqolate.com>
2014-04-17 02:00:51 +0900

    Bump version to 0.5.0.

--- app/src/main/java/jp/co/cyberagent/stf/Agent.java
+++ app/src/main/java/jp/co/cyberagent/stf/Agent.java
@@ -18,7 +18,7 @@ import jp.co.cyberagent.stf.compat.WindowManagerWrapper;
 import jp.co.cyberagent.stf.util.InternalApi;
 
 public class Agent {
-    public static final String VERSION = "0.4.1";
+    public static final String VERSION = "0.5.0";
     public static final int PORT = 1090;
 
     private InputManagerWrapper inputManager;
