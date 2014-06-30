f167a33008eec479327b0a1d72862d3f08a61e30
Valverde Antonio <valverde_antonio@cyberagent.co.jp>
2014-06-30 18:34:21 +0900

    Implement get Wifi status.

---
+++ app/src/main/java/jp/co/cyberagent/stf/query/GetWifiStatusResponder.java
@@ -0,0 +1,38 @@
+package jp.co.cyberagent.stf.query;
+
+import android.content.Context;
+import android.net.wifi.WifiManager;
+
+import com.google.protobuf.GeneratedMessage;
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import jp.co.cyberagent.stf.proto.Wire;
+
+public class GetWifiStatusResponder extends AbstractResponder {
+    public GetWifiStatusResponder(Context context) {
+        super(context);
+    }
+
+    @Override
+    public GeneratedMessage respond(Wire.Envelope envelope) throws InvalidProtocolBufferException {
+        Wire.GetWifiStatusRequest request =
+                Wire.GetWifiStatusRequest.parseFrom(envelope.getMessage());
+
+        WifiManager wm = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+
+        return Wire.Envelope.newBuilder()
+                .setId(envelope.getId())
+                .setType(Wire.MessageType.GET_WIFI_STATUS)
+                .setMessage(Wire.GetWifiStatusResponse.newBuilder()
+                        .setSuccess(true)
+                        .setStatus(wm.isWifiEnabled())
+                        .build()
+                        .toByteString())
+                .build();
+    }
+
+    @Override
+    public void cleanup() {
+        // No-op
+    }
+}
