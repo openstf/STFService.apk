5bcc5a3c5fe1345e8c8ddd2c409a3f74b27cfb33
Simo Kinnunen <simo@shoqolate.com>
2014-11-10 16:46:44 +0900

    Make UnlockActivity work on newer Android devices. Planning on using KeyguardLock for older ones.

--- app/src/main/java/jp/co/cyberagent/stf/UnlockActivity.java
+++ app/src/main/java/jp/co/cyberagent/stf/UnlockActivity.java
@@ -3,6 +3,7 @@ package jp.co.cyberagent.stf;
 import android.app.Activity;
 import android.app.KeyguardManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -13,11 +14,23 @@ public class UnlockActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         Window window = getWindow();
-        window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+
+        // Don't add FLAG_SHOW_WHEN_LOCKED, it usually causes the screen to get locked
+        // again when this activity is finished.
         window.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
         window.addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
 
-        KeyguardManager keyguardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
-        keyguardManager.newKeyguardLock("InputService").disableKeyguard();
+        // Just in case we can't finish quickly enough, let's at the very least let
+        // any events through.
+        window.addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+        window.addFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
+
+        // Close ASAP.
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                UnlockActivity.this.finish();
+            }
+         }, 500);
     }
 }
