736c40dfd4fd3a2ce6f19800f597e0adfddc3aaf
Simo Kinnunen <simo@shoqolate.com>
2016-09-03 03:58:24 +0900

    Bump to 1.1.0.

--- app/src/main/java/jp/co/cyberagent/stf/Version.java
+++ app/src/main/java/jp/co/cyberagent/stf/Version.java
@@ -6,5 +6,5 @@ public class Version {
      * the same value from non-Context code as well, so to keep things easy, we'll just use
      * a plain string as our version number.
      */
-    public static final String name = "1.0.2";
+    public static final String name = "1.1.0";
 }
